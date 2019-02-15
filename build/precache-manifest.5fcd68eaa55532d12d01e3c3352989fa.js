8f5159691b0589157cc4cc903d5d9b97da8f8db8
Profesor08 <online7890@gmail.com>
2019-02-15 03:05:04 +0200

    Added build

---
+++ build/precache-manifest.5fcd68eaa55532d12d01e3c3352989fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b9d477c3c96190fbe6c4",
+    "url": "/static/js/main.b9d477c3.chunk.js"
+  },
+  {
+    "revision": "6b78c74ac7acb2ce7e2d",
+    "url": "/static/js/2.6b78c74a.chunk.js"
+  },
+  {
+    "revision": "b9d477c3c96190fbe6c4",
+    "url": "/static/css/main.606f716a.chunk.css"
+  },
+  {
+    "revision": "288849ba2522d34dcee22fb482062a2b",
+    "url": "/index.html"
+  }
+];
